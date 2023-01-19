47c935f38af158e44578a746dc3981b1a2b1e346
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-19 13:13:32 +0200

    corrected homework

--- RecursiveExponentiation.js
+++ RecursiveExponentiation.js
@@ -6,22 +6,18 @@
 // функцію pow(num, degree).
 const numberOfPrompt = prompt(`Enter number for exponentiation`)
 const exponentiationOfPrompt = prompt(`Enter exponentiation`)
-if (numberOfPrompt === null || numberOfPrompt.trim() === "" ||
-exponentiationOfPrompt === null || exponentiationOfPrompt .trim() === "" ||
-isNaN(+numberOfPrompt) === true || isNaN(+exponentiationOfPrompt) === true) {
+if (numberOfPrompt?.trim() && exponentiationOfPrompt ?.trim() &&
+    !isNaN(+numberOfPrompt) && !isNaN(+exponentiationOfPrompt) &&
+    exponentiationOfPrompt >= 0){
+    const number1 = +numberOfPrompt
+    const exponentiation1 = +exponentiationOfPrompt
+    function recursivePow(number, exponentiation) {
+        if (number === 0) return 1
+        if (exponentiation === 0) return 1
+        return number * recursivePow(number, (exponentiation - 1));
+    }
+    alert(recursivePow(number1, exponentiation1))
+}
+else {
     alert(`Entered number is not validation`)
 }
-/*
-if (!number ?.trim() || !exponentiation ?.trim()){
-    alert(`Entered number is not validation`)
-}  */
-const number = +numberOfPrompt
-const exponentiation = +exponentiationOfPrompt
-function recursivePow(number, exponentiation) {
-    if (number === 0) return 1
-    if (exponentiation === 0) return 1
-    if (!number) return 0
-    if (!exponentiation) return 0
-    return number * recursivePow(number, (exponentiation - 1));
-}
-alert(recursivePow(number, exponentiation))
